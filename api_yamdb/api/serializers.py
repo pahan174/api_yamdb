4ae946a29ac56adef1c751d70b58a8db4e3c264b
Pavel U <pahan174@gmail.com>
2022-05-11 15:39:41 +0500

    сериализаторы

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,9 @@
+from pyexpat import model
+from attr import fields
 from rest_framework import serializers
 
 from users.models import CustomUser
+from reviews.models import Review, Comment
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -8,3 +11,20 @@ class CustomUserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ('id', 'username', 'email')
         ref_name = 'ReadOnlyUsers'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Review
